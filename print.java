1ef257750343cb8c4abfe64b43d6e2cf26fe6871
Nowfa143 <95207574+Nowfa143@users.noreply.github.com>
2023-07-22 18:27:36 +0530

    Update print.java

--- print.java
+++ print.java
@@ -1,6 +1,5 @@
 class java {
     public static void main(String args[]) {
         System.out.println("first printing line");
-        System.out.println("second printing line");
-    }
+       }
 }
